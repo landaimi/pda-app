8171503e5f75c0e46d960c7efac9d8767c1b4104
zhangff <zhangff@wanhuhealth.com>
2018-12-20 11:09:16 +0800

    feat: new page

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import Login from './Screen/Login';
-import Main from './Screen/Main';
-import Test from './Screen/Test';
+import Home from './Screen/Home';
+import BarcodeScannerView from './Screen/BarcodeScannerView';
+import CheckInfo from './Screen/CheckInfo';
+import InventoryInfo from './Screen/InventoryInfo';
+import MaintainInfo from './Screen/MaintainInfo';
 
-const TabNavigator = createBottomTabNavigator({
-  Login: Login,
-  Main: Main,
-  Test: Test
+const TabNavigator = createSwitchNavigator({
+  Login: {
+    screen: Login,
+  },
+  Home: {
+    screen: Home,
+  },
+  Scanner: {
+    screen: BarcodeScannerView,
+  },
+  CheckInfo: {
+    screen: CheckInfo,
+  },
+  InventoryInfo: {
+    screen: InventoryInfo,
+  },
+  MaintainInfo: {
+    screen: MaintainInfo,
+  },
 });
 
 export default createAppContainer(TabNavigator);
